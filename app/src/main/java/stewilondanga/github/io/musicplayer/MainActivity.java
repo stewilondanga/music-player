bb5eacca74738e35e99a167839eedc900bd57ba9
Stewart ilondanga <stilondanga@gmail.com>
2019-08-16 18:31:47 +0300

    add app/src/main/java/stewilondanga/github/io/musicplayer/MainActivity.java

--- app/src/main/java/stewilondanga/github/io/musicplayer/MainActivity.java
+++ app/src/main/java/stewilondanga/github/io/musicplayer/MainActivity.java
@@ -6,3 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound);
+        Button playButton = (Button) findViewById(R.id.start);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        Toast.makeText(MainActivity.this, "The Song is Over", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.stop);
+
+        pauseButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
